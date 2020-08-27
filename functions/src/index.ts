99cb9555d388b02fe2c6f467ef5f4423f673f790
Blank <s180283@student.dhbw-mannheim.de>
2020-08-27 18:03:44 +0200

    created sepereate cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,23 @@
+'use strict';
 import * as functions from 'firebase-functions';
+import * as trainingModule from './training';
+import * as caloriesModule from './calories';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
+
+export const calories: functions.HttpsFunction = functions
+  .region('europe-west2')
+  .https.onRequest(
+    (request: functions.https.Request, response: functions.Response) => {
+      caloriesModule.handler(request, response);
+    }
+  );
+
+export const training: functions.HttpsFunction = functions
+  .region('europe-west2')
+  .https.onRequest(
+    (request: functions.https.Request, response: functions.Response) => {
+      trainingModule.handler(request, response);
+    }
+  );
---
+++ functions/src/training.ts
@@ -0,0 +1,8 @@
+import * as functions from 'firebase-functions';
+export function handler(
+  request: functions.https.Request,
+  response: functions.Response
+): void {
+  console.log(request);
+  response.send('Hello from training!');
+}
