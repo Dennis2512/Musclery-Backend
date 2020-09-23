43640fee880871b8e37d42ae1195ba9df652b88a
Arthur Siebert <arthursiebert@arthurs-imac.lan>
2020-09-23 19:09:52 +0200

    commit

--- functions/src/calories.ts
+++ functions/src/calories.ts
@@ -1,9 +1,56 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { food } from './models/food';
 export const calories: functions.HttpsFunction = functions
   .region('europe-west2')
   .https.onRequest(
-    (request: functions.https.Request, response: functions.Response) => {
-      response.send('Hello from calories!');
-      
+    async (
+      request: functions.https.Request,
+      response: functions.Response
+    ): Promise<void> => {
+      switch (request.method) {
+        case 'GETALL':
+          await getEveryFood(response);
+          break;
+        case 'GET':
+          await getFood(request, response);
+          break;
+        default:
+          response.status(405).send('Method not allowed.');
+      }
     }
   );
+
+  //functions
+
+  async function getEveryFood(response: functions.Response): Promise<void>{
+    try {
+      const docs: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData> = await admin
+      .firestore()
+      .collection('Food-DB')
+      .get();
+      const foodL: food[] = [];
+      docs.forEach((doc) => {
+        foodL.push(
+          new food(
+            doc.id,
+            doc.get('name'),
+            doc.get('carbsPMG'),
+            doc.get('category'),
+            doc.get('hint'),
+            doc.get('nutriScore'),
+            doc.get('proteinsPMG'),
+            doc.get('barcode'),
+            doc.get('calPMG'),
+            doc.get('fatPMG')
+          )
+        )
+      })
+    } catch (e) {
+      response.status(400).send('damn');
+    }
+  }
+
+  async function getFood(request: functions.Request, response: functions.Response): Promise<void>{
+
+  }
--- functions/src/models/food.ts
+++ functions/src/models/food.ts
@@ -1,5 +1,5 @@
 export class food {
-    private id?: string;
+    private _id?: string;
     private name?: string;
     private carbsPMG?: number;
     private category?: string;
@@ -14,16 +14,114 @@ export class food {
     constructor(
       id?: string,
       name?: string,
+      carbsPMG?: number,
+      category?: string,
+      hint?: string,
+      nutriScore?: number,
+      proteinsPMG?: number,
+      barcode?: string,
+      calPMG?: number,
+      fatPMG?: number,
     ) {
-      this.id = id;
+      this._id = id;
       this.name = name;
+      this.calPMG = calPMG;
+      this.carbsPMG = carbsPMG;
+      this.category = category;
+      this.hint = hint;
+      this.nutriScore = nutriScore;
+      this.proteinsPMG =  proteinsPMG;
+      this.barcode = barcode;
+      this.fatPMG = fatPMG;
     }
   
     // setter
+    
+    
+    public setId(value: string) {
+      this._id = value;
+    }
+    
+    public setName(value: string) {
+      this.name = value;
+    }
+    
+    public setCalPMG(value: number) {
+      this.calPMG = value;
+    }
+    
+    public setCarbsPMG(value: number) {
+      this.carbsPMG = value;
+    }
+    
+    public setCategory(value: string) {
+      this.category = value;
+    }
+    
+    public setHint(value: string) {
+      this.hint = value;
+    }
+    
+    public setNutriScore(value: number) {
+      this.nutriScore = value;
+    }
+    
+    public setProteinsPMG(value: number) {
+      this.proteinsPMG = value;
+    }
+    
+    public setBarcode(value: string) {
+      this.barcode = value;
+    }
+    
+    public setfatPMG(value: number) {
+      this.fatPMG = value;
+    }
   
- 
+    
+    
   
     // getter
-  
+    public getId(): string | undefined{
+      return this._id;
+    }
+
+    public getName(): string | undefined{
+      return this.name;
+    }
+
+    public getCalPMG(): number | undefined{
+      return this.calPMG;
+    }
+
+    public getCarbsPMG(): number | undefined{
+      return this.carbsPMG;
+    }
+
+    public getCategory(): string | undefined{
+      return this.category;
+    }
+
+    public getHint(): string | undefined{
+      return this.hint;
+    }
+
+    public getNutriScore(): number | undefined{
+      return this.nutriScore;
+    }
+
+    public getProteinsPMG(): number | undefined{
+      return this.proteinsPMG;
+    }
+
+    public getBarcode(): string | undefined{
+      return this.barcode;
+    }
+
+    public getfatPMG(): number | undefined{
+      return this.fatPMG;
+    }
+
+    
   }
   
